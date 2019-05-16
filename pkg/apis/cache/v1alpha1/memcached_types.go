dfc674b0f03c1d832a8ec748b84408de53fbe7fc
Carlos Venegas <vmjcarlos@gmail.com>
2019-05-15 23:39:12 -0500

    api: Add state values
    Add spec and status values for Memcached api kind
    
    Signed-off-by: Carlos Venegas <vmjcarlos@gmail.com>

--- pkg/apis/cache/v1alpha1/memcached_types.go
+++ pkg/apis/cache/v1alpha1/memcached_types.go
@@ -10,18 +10,17 @@ import (
 // MemcachedSpec defines the desired state of Memcached
 // +k8s:openapi-gen=true
 type MemcachedSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
-	// Add custom validation using kubebuilder tags: https://book.kubebuilder.io/beyond_basics/generating_crd.html
+	// Size is the size of the memcached deployment
+	Size int32 `json:"size"`
 }
 
 // MemcachedStatus defines the observed state of Memcached
 // +k8s:openapi-gen=true
 type MemcachedStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
-	// Add custom validation using kubebuilder tags: https://book.kubebuilder.io/beyond_basics/generating_crd.html
+	// Nodes are the names of the memcached pods
+	Nodes []string `json:"nodes"`
 }
+
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 
